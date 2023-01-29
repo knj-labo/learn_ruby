45526cbc5da8c9ff0b823de3c729ed2eeffe35f4
jp-knj <kenji.tomita1996@gmail.com>
2023-01-29 15:30:48 +0900

    :tada: learn best practices for implementing email password authentication on a graphql server written in ruby

--- graphql-tutorial/app/graphql/types/mutation_type.rb
+++ graphql-tutorial/app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :signin_user, mutation: Mutations::SignInUser
     field :create_user, mutation: Mutations::CreateUser
     field :create_link, mutation: Mutations::CreateLink
   end

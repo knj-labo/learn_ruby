60e354d4fa01692489c01d42d5ccc79822ab0ead
jp-knj <kenji.tomita1996@gmail.com>
2023-01-29 15:45:11 +0900

    :sparkles: implementing advanced graphql mutations with ruby and graphql-ruby

--- graphql-tutorial/app/graphql/types/mutation_type.rb
+++ graphql-tutorial/app/graphql/types/mutation_type.rb
@@ -2,6 +2,7 @@ module Types
   class MutationType < Types::BaseObject
     field :signin_user, mutation: Mutations::SignInUser
     field :create_user, mutation: Mutations::CreateUser
+    field :create_vote, mutation: Mutations::CreateVote
     field :create_link, mutation: Mutations::CreateLink
   end
 end

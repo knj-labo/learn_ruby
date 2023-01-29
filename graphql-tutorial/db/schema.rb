6a7ec4f5512773671d7a88d4927a70efb4b4a9fe
jp-knj <kenji.tomita1996@gmail.com>
2023-01-29 15:00:45 +0900

    :sparkles: run test

--- graphql-tutorial/db/schema.rb
+++ graphql-tutorial/db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_22_125143) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_29_052631) do
   create_table "links", force: :cascade do |t|
     t.string "url"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

0051ce2d4d9f143132a0483a4b1c084b9b13cd0d
jp-knj <kenji.tomita1996@gmail.com>
2023-04-16 22:28:39 +0900

    :sparkles: learn symbol and string

--- polished-ruby-programming/01_Chaptor/00-chapter.rb
+++ polished-ruby-programming/01_Chaptor/00-chapter.rb
@@ -82,3 +82,97 @@ end
 # --
 
 cache.fetch(:key){cache[:key] = some_expression}
+
+## Different numeric types for different needs
+
+10.times do
+  # executed 10 times
+end
+
+# --
+
+5 / 10
+# => 0
+
+7 / 3
+# => 2
+
+# --
+
+5 / 10r # or Rational(5, 10) or 5 / 10.to_r
+# => (1/2)
+
+7.0 / 3
+# => 2.3333333333333335
+
+# --
+
+f = 1.1
+v = 0.0
+1000.times do
+  v += f
+end
+v
+# => 1100.0000000000086
+
+# --
+
+f = 1.109375
+v = 0.0
+1000.times do
+  v += f
+end
+v
+# => 1109.375
+
+# --
+
+f = 1.1r
+v = 0.0r
+1000.times do
+  v += f
+end
+v
+# => (1100/1)
+
+# --
+
+f = 1.109375r
+v = 0.0r
+1000.times do
+  v += f
+end
+v
+# => (8875/8)
+v.to_f
+# => 1109.375
+
+# --
+
+v = BigDecimal(1)/3
+v * 3
+# => 0.999999999999999999e0
+
+# --
+
+f = BigDecimal(1.1, 2)
+v = BigDecimal(0)
+1000.times do
+  v += f
+end
+v
+# => 0.11e4
+v.to_s('F')
+# => "1100.0"
+
+# --
+
+f = BigDecimal(1.109375, 7)
+v = BigDecimal(0)
+1000.times do
+  v += f
+end
+v
+# => 0.1109375e4
+v.to_s('F')
+# => "1109.375"

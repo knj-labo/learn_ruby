9df3ebd63121e9e27c27a3b225c488945c7f6983
jp-knj <kenji.tomita1996@gmail.com>
2023-05-02 01:53:52 +0900

    :+1: add MonthlySchdule class details: should have a word_hours_for method that takes a date and returns the work hours for that date, and a holiday method or attribute that returns an array of holiday dates for the month

---
+++ polished-ruby-programming/02_Chaptor/08-di-v1.rb
@@ -0,0 +1,40 @@
+class CurrentDay
+  def initialize(date: Date.today)
+    @date = date
+    @schedule = MonthlySchedule.new(date.year, date.month)
+  end
+
+  def work_hours
+    @schedule.work_hours_for(@date)
+  end
+
+  def workday?
+    !@schedule.holidays.include?(@date)
+  end
+end
+
+class MonthlySchedule
+  attr_reader :holidays
+
+  def initialize(year, month)
+    @year = year
+    @month = month
+    @holidays = calculate_holidays
+    @work_hours = default_work_hours
+  end
+
+  def work_hours_for(date)
+    @work_hours[date.day - 1]
+  end
+
+  private
+
+  def default_work_hours
+    Array.new(Date.new(@year, @month, -1).day, 8)
+  end
+
+  def calculate_holidays
+    # Replace this with your actual holiday calculation logic
+    [Date.new(@year, @month, 1), Date.new(@year, @month, 25)]
+  end
+end

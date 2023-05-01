d9b9f8e630feec6a9ef0e08e680f0a1c47b08981
jp-knj <kenji.tomita1996@gmail.com>
2023-05-02 00:01:04 +0900

    :sparkles: returns the build text as a string

--- polished-ruby-programming/02_Chaptor/04-single-responsibility-builder-modifier.rb
+++ polished-ruby-programming/02_Chaptor/04-single-responsibility-builder-modifier.rb
@@ -5,3 +5,10 @@ builder.append("ing...1...2")
 # prints the built text, which would be:
 # puts builder.to_s
 # testing...1...2
+
+builder.append(". Found: ")
+builder.append(name)
+
+# The output would be:
+# testing...1...2. Found: Amazing_person
+puts builder.as_string

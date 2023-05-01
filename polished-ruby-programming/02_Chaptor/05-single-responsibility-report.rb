e8bd68f44bd821faa45ee1cbbbf740829394c35a
jp-knj <kenji.tomita1996@gmail.com>
2023-05-02 00:13:59 +0900

    :sparkles: create ReportContent class to store the report data

--- polished-ruby-programming/02_Chaptor/05-single-responsibility-report.rb
+++ polished-ruby-programming/02_Chaptor/05-single-responsibility-report.rb
@@ -13,3 +13,13 @@ end
 data = "Sample report data"
 report = Report.new(data)
 puts report.format
+
+class ReportContent
+  # Creates instance variables and corresponding methods that return the value of each instance variable.
+  # Equivalent to calling “attr:name'' on each name in turn. String arguments are converted to symbols.
+  attr_reader :data
+
+  def initialize(data)
+    @data = data
+  end
+end

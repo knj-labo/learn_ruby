aaf22979152a4d264b55a325f29c2506ecbbf266
jp-knj <kenji.tomita1996@gmail.com>
2023-05-02 00:58:48 +0900

    :sparkles: add Max class details: two different versions of a MaxBy subclass. the Max class has a method over? which takes a number n and check if its greater than the @max attribute

---
+++ polished-ruby-programming/02_Chaptor/07-liskov.rb
@@ -0,0 +1,39 @@
+class Max
+  def initialize(max)
+    @max = max
+  end
+
+  def over?(n)
+    @max < n
+  end
+end
+
+class MaxBy < Max
+  def over?(n, by)
+    @max + by < n
+  end
+end
+
+class MaxBy < Max
+  def over?(n, by: 0)
+    @max + by < n
+  end
+end
+
+if obj.instance_of?(Max)
+  # do something
+else
+  # do something else
+end
+
+if obj.class == Max
+  # do something
+else
+  # do something else
+end
+
+if obj.kind_of?(Max)
+  # do something
+else
+  # do something else
+end

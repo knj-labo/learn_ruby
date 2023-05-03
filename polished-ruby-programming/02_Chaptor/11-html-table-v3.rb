5b54fa2f758cda57ab49ebcbc5da5b2fde5e7f20
jp-knj <kenji.tomita1996@gmail.com>
2023-05-03 23:04:03 +0900

    :sparkles: update it to generate table omponent

--- polished-ruby-programming/02_Chaptor/11-html-table-v3.rb
+++ polished-ruby-programming/02_Chaptor/11-html-table-v3.rb
@@ -7,4 +7,26 @@ class HTMLTable
     yield
     html << "</" << type << ">"
   end
+
+  # initialize(rows): Initializes the HTMLTable object with the given array of rows.
+  # This method generates the HTML string representation of the table.
+
+  # It uses nested wrap method calls to create the table, tbody, tr, and td elements while iterating through the rows and cells.
+  # The cell content is escaped using CGI.escapeHTML before being appended to the html string.
+  def to_s
+    html = String.new
+    wrap(html, 'table') do
+      wrap(html, 'tbody') do
+        @rows.each do |row|
+          wrap(html, 'tr') do
+            row.each do |cell|
+              wrap(html, 'td') do
+                html << CGI.escapeHTML(cell.to_s)
+              end
+            end
+          end
+        end
+      end
+    end
+  end
 end
